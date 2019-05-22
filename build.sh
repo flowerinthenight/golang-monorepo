e82183ce5507235e7d0b00c4b8715f6371190fd1
flowerinthenight <chew.esmero@mobingi.com>
2019-05-22 11:00:19 +0900

    check before docker install

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Somehow, this is important even though we already have `setup_remote_docker`.
-VER="17.12.0-ce"
-curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
-tar -xz -C /tmp -f /tmp/docker-$VER.tgz
-mv /tmp/docker/* /usr/bin
-docker --version
+docker --version; rc=$?
+if [ $rc -ne 0 ]; then
+  # Somehow, this is important even though we already have `setup_remote_docker`.
+  VER="18.06.1-ce"
+  curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
+  tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+  mv /tmp/docker/* /usr/bin
+  docker --version
+fi
 
 ### Install awscli. Used for pushing container images to ECR.
 ### You may want to set these environment variables:
